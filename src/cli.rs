df66e9ebb4002f1af3857859371c00bdc6f591bf
Dan Bond <danbond@protonmail.com>
2022-02-02 16:38:38 -0800

    clap disable default features

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{app_from_crate, App, AppSettings, ArgMatches, ColorChoice};
+use clap::{app_from_crate, App, AppSettings, ArgMatches};
 
 use crate::{install, start};
 
@@ -6,8 +6,6 @@ pub fn new() -> ArgMatches {
     app_from_crate!()
         .global_setting(AppSettings::PropagateVersion)
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::HidePossibleValues)
-        .color(ColorChoice::Never)
         .subcommand(App::new(install::COMMAND_NAME).about("Install"))
         .subcommand(App::new(start::COMMAND_NAME).about("Start"))
         .get_matches()

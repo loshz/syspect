39e88671c1335deb3c72531941ce08adcee38e8b
Dan Bond <danbond@protonmail.com>
2022-10-25 13:45:46 -0400

    fix bpf tracepoint
    Signed-off-by: Dan Bond <danbond@protonmail.com>

--- bpf/tracepoints/sys_enter.bpf.c
+++ bpf/tracepoints/sys_enter.bpf.c
@@ -6,23 +6,22 @@ struct {
   __uint(type, BPF_MAP_TYPE_ARRAY);
   __type(key, u32);
   __type(value, u64);
-  __uint(max_entries, 1000);
+  __uint(max_entries, 1);
 } syscall_count SEC(".maps");
 
 // This tracepoint is defined in:
 // /sys/kernel/debug/tracing/events/raw_syscalls/sys_enter
 SEC("tracepoint/raw_syscalls/sys_enter")
-int sys_enter(struct bpf_raw_tracepoint_args *ctx) {
-  struct pt_regs *regs;
-  const char *key;
-  u64 count = 1;
+int sys_enter() {
+  u32 key = 0;
+  u64 init_val = 1, *count;
 
-  // Get value from CPU register.
-  regs = (struct pt_regs *)ctx->args[0];
-  bpf_probe_read(&key, sizeof(key), &regs->si);
-
-  // Write key=program, value=count to the map.
-  bpf_map_update_elem(&syscall_count, &key, &count, BPF_ANY);
+  count = bpf_map_lookup_elem(&syscall_count, &key);
+  if (!count) {
+    bpf_map_update_elem(&syscall_count, &key, &init_val, BPF_ANY);
+    return 0;
+  }
+  __sync_fetch_and_add(count, 1);
 
   return 0;
 }

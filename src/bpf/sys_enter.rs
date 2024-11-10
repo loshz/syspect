86b5352878a7dbb49e6fd2c710a2fa4ca039b75e
Dan Bond <danbond@protonmail.com>
2024-11-10 21:57:00 +0000

    bpf: init tracepoint
    Signed-off-by: Dan Bond <danbond@protonmail.com>

--- src/bpf/sys_enter.rs
+++ src/bpf/sys_enter.rs
@@ -1,3 +1,6 @@
+use std::mem::MaybeUninit;
+
+use libbpf_rs::skel::{OpenSkel, Skel, SkelBuilder};
 use prometheus_client::{
     collector::Collector,
     metrics::{family::Family, gauge::Gauge},
@@ -28,7 +31,18 @@ impl Programmable for SysEnter {
     fn run(&self) -> Result<(), Error> {
         println!("sys_enter");
 
-        // TODO: get actual sys_enter count.
+        let sys_enter = SysEnterSkelBuilder::default();
+        let mut open_object = MaybeUninit::uninit();
+        let prog = sys_enter
+            .open(&mut open_object)
+            .map_err(|_| Error::Program("failed to open bpf prog `sys_enter`".into()))?;
+        let mut tracepoint = prog
+            .load()
+            .map_err(|_| Error::Program("failed to load bpf prog `sys_enter`".into()))?;
+        tracepoint
+            .attach()
+            .map_err(|_| Error::Program("failed to attach bpf prog `sys_enter`".into()))?;
+
         self.totals
             .get_or_create(&ProcessLabels {
                 pid: 1,
